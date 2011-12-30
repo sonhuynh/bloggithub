f5fc2f711b6d4da08f30e11b2996b36fd50d1603
sonhuynh <son.huynh@techpropulsionlabs.com>
2011-12-30 11:19:33 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogheroku::Application.config.secret_token = '112fb3daf8d02bb450f4134376b200062af5ca704ef0c4cc4a255a80236b84cfe8c6a8f315acf48f815ae659c6e1317db7754990fd807056d0a9d039818f3a2a'
